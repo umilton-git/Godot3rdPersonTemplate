792d96c163cf19b160cf2aeb913d20a3d382a07e
Illiad <ulysses.github@protonmail.com>
2023-12-23 20:23:59 -0500

    Initial commit with full template

---
+++ Testia/Scripts/Movement.gd
@@ -0,0 +1,42 @@
+extends CharacterBody3D
+
+var movement_speed
+var walk_speed = 5.0
+var run_speed = 10.0
+
+var vertical_velocity = 0
+var gravity = 30
+
+var acceleration = 6
+var angular_acceleration = 7
+var jump_impulse = 30
+
+
+func get_input(delta):
+	var h_rot = $CamRoot/h.global_transform.basis.get_euler().y
+	var input = Input.get_vector("move_left", "move_right", "move_forward", "move_back") * Vector2(-1, -1)
+	var direction = Vector3(input.x, 0, input.y).rotated(Vector3.UP, h_rot).normalized()
+
+	if direction.length() > 0:
+		$TestCube.rotation.y = lerp_angle($TestCube.rotation.y, atan2(direction.x, direction.z), delta * angular_acceleration)
+		if Input.is_action_pressed('sprint'):
+			movement_speed = run_speed
+		else:
+			movement_speed = walk_speed
+	else:
+		movement_speed = 0
+		
+	if Input.is_action_just_pressed('Jump') and is_on_floor():
+		velocity.y = jump_impulse
+
+	var target_velocity = direction * movement_speed
+	velocity = velocity.lerp(target_velocity, delta * acceleration)	
+
+func _physics_process(delta):
+	if !is_on_floor():
+		velocity.y -= gravity * delta
+	else:
+		velocity.y = 0
+		
+	get_input(delta)
+	move_and_slide()
